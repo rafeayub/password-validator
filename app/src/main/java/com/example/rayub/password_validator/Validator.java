7635f91f48857fff942b725c67b3d5b0e34facb6
rafeayub <rf236743@dal.ca>
2018-06-07 17:47:38 -0300

    added code for stage 1 of question 1

--- app/src/main/java/com/example/rayub/password_validator/Validator.java
+++ app/src/main/java/com/example/rayub/password_validator/Validator.java
@@ -4,32 +4,51 @@ public class Validator {
 
     private String word;
 
-    public Validator(String word){
+    public Validator(String word) {
         this.word = word;
     }
 
-    public void setPassword(String word)
-    {
+    public void setPassword(String word) {
         this.word = word;
     }
 
-     public String getPassword()
-     {
+    public String getPassword() {
         return this.word;
-     }
+    }
 
-     public boolean isValid()
-     {
-         if (!word.toLowerCase().equals("password")) //makes sure password is not "password" (case insensitive)
-         {
-              return false;
-         }
-
-         if (word.length() < 8) //less than 8 characters
-         {
+    public boolean isValid() {
+        if (!word.toLowerCase().equals("password")) //make sure password is not "password" (case insensitive)
+        {
             return false;
-         }
+        }
 
-         return true;
-     }
+        if (word.length() < 8) //less than 8 characters
+        {
+            return false;
+        }
+
+        //Custom Rules
+        if (word.length() > 20) //Custom Rule 1: can't be longer than 20 characters
+        {
+            return false;
+        }
+
+        for (int i = 0; i < word.length(); i++) //Custom Rule 2: can't be a '?'
+        {
+            if (word.charAt(i) == '?')
+            {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < word.length(); i++) //Custom Rule 3: no digits
+        {
+            if (Character.isDigit(word.charAt(i)))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
